00c81c40275a173b0c64492f563f741f665100ff
Gabriel <zjryane@gmail.com>
2016-07-14 18:43:08 +0800

    频道路由设置进入权限，现在无权限用户不能浏览高于其权限的频道了

--- app/controllers/views.py
+++ app/controllers/views.py
@@ -2,9 +2,11 @@ from flask import render_template
 
 from ..models import Channel
 from . import controllers
+from ..decorators import channel_permission_required
 
 
 @controllers.route('/channel/<int:id>')
+@channel_permission_required
 def channel_view(id):
     channel = Channel.channel_by_id(id)
     posts = channel.posts
--- app/decorators.py
+++ app/decorators.py
@@ -1,6 +1,7 @@
 from flask import abort
 from functools import wraps
 from .models import current_user
+from .models import Channel
 
 
 def login_required(f):
@@ -8,5 +9,21 @@ def login_required(f):
     def wrapped(*args, **kwargs):
         if current_user() is None:
             abort(405)
+        return f(*args, **kwargs)
+    return wrapped
+
+
+def channel_permission_required(f):
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        channel_id = kwargs.get('id')
+        channel = Channel.channel_by_id(channel_id)
+        user = current_user()
+        if user is None:
+            permission = 0x01
+        else:
+            permission = user.permissions()
+        if permission & channel.permission == 0:
+            abort(401)
         return f(*args, **kwargs)
     return wrapped
